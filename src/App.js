f73fdc2ac510d432bb7f5da9a534c295bfcb33c0
Prince <veertyagia@gmail.com>
2021-06-02 20:38:04 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from './componets/layouts/Header'
+import Main from './componets/layouts/Main';
+function App() {
+  return (
+    <div className="App">
+       <Header/> 
+       <Main/> 
+    </div>
+  );
+}
+
+export default App;
---
+++ src/temp/EmailData.js
@@ -0,0 +1,80 @@
+export const emailData = [
+    {
+        starred: true,
+        from: 'Prince',
+        subject: 'Yoooo',
+        message: 'sup bro',
+        received: '08:20',
+        read: true
+    }, {
+        starred: true,
+        from: 'Samar',
+        subject: 'Yoooo',
+        message: 'sup broooooo',
+        received: '08:30',
+        read: true
+    }, {
+        starred: false,
+        from: 'Vishal',
+        subject: 'Yoooo',
+        message: 'sup breh',
+        received: '09:25',
+        read: true
+    }, {
+        starred: true,
+        from: 'Vishal',
+        subject: 'Yoooo',
+        message: 'sup breh',
+        received: '09:25',
+        read: false
+    }, {
+        starred: true,
+        from: 'Vishal',
+        subject: 'Yoooo',
+        message: 'sup breh',
+        received: '09:25',
+        read: false
+    }, {
+        starred: false,
+        from: 'Vishal',
+        subject: 'Yoooo',
+        message: 'sup breh',
+        received: '09:25',
+        read: true
+    }, {
+        starred: false,
+        from: 'Vishal',
+        subject: 'Yoooo',
+        message: 'sup breh',
+        received: '09:25',
+        read: false
+    }, {
+        starred: false,
+        from: 'Vishal',
+        subject: 'Yoooo',
+        message: 'sup breh',
+        received: '09:25',
+        read: false
+    },{
+        starred: false,
+        from: 'Vishal',
+        subject: 'Yoooo',
+        message: 'sup breh',
+        received: '09:25',
+        read: true
+    },{
+        starred: false,
+        from: 'Vishal',
+        subject: 'Yoooo',
+        message: 'sup breh',
+        received: '09:25',
+        read: false
+    },{
+        starred: false,
+        from: 'Vishal',
+        subject: 'Yoooo',
+        message: 'sup breh',
+        received: '09:25',
+        read: false
+    },
+]
